0abb88fb5ffda12a9c98f140d9cff0c79a5e8f9f
HarisShaikh97 <harisahmedsheikh1997@gmail.com>
2024-10-29 22:54:46 +0500

    Add job layout updated

--- app/add-job/_layout.tsx
+++ app/add-job/_layout.tsx
@@ -1,49 +1,19 @@
 import {
 	KeyboardAvoidingView,
 	ScrollView,
-	View,
-	Text,
 	Platform,
 	StyleSheet
 } from "react-native"
-import { ImageBackground } from "expo-image"
-import { useFonts } from "expo-font"
 import { Slot } from "expo-router"
-import BackButton from "../../components/back-button/BackButton"
-import { theme } from "../../utils/constants"
 
 export default function Layout(): React.ReactElement | null {
-	const [fontsLoaded] = useFonts({
-		"Montserrat-Bold": require("../../assets/fonts/Montserrat/Montserrat Bold 700.ttf")
-	})
-
 	return (
 		<KeyboardAvoidingView
 			style={styles.scrollView}
 			behavior={Platform.OS === "ios" ? "padding" : "height"}
 		>
 			<ScrollView showsVerticalScrollIndicator={false}>
-				<View style={styles.container}>
-					<ImageBackground
-						source={require("../../assets/images/add-job-header.png")}
-						style={styles.headerBackgroundImage}
-						contentFit="fill"
-					>
-						<View style={styles.headerContainer}>
-							<BackButton
-								color="#000000"
-								backgroundColor="#F5F5F5"
-								borderColor="transparent"
-							/>
-							{fontsLoaded && (
-								<Text style={styles.titleText}>
-									Describe Your Job Needs
-								</Text>
-							)}
-						</View>
-					</ImageBackground>
-					<Slot />
-				</View>
+				<Slot />
 			</ScrollView>
 		</KeyboardAvoidingView>
 	)
@@ -52,25 +22,6 @@ export default function Layout(): React.ReactElement | null {
 const styles = StyleSheet.create({
 	scrollView: {
 		flex: 1,
-		flexDirection: "column",
 		backgroundColor: "white"
-	},
-	container: {
-		flexDirection: "column"
-	},
-	headerBackgroundImage: {
-		width: "100%"
-	},
-	headerContainer: {
-		padding: 25,
-		flexDirection: "column",
-		gap: 25,
-		backgroundColor: "rgba(255, 255, 255, 0.75)"
-	},
-	titleText: {
-		fontSize: 27.5,
-		fontFamily: "Montserrat-Bold",
-		color: theme.colors.secondary,
-		width: 215
 	}
 })
--- app/add-job/index.tsx
+++ app/add-job/index.tsx
@@ -1,13 +1,20 @@
 import { useState, useCallback } from "react"
-import { View, StyleSheet } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
+import { ImageBackground } from "expo-image"
+import { useFonts } from "expo-font"
 import { useRouter } from "expo-router"
+import BackButton from "../../components/back-button/BackButton"
 import InputField from "../../components/input-field/InputField"
 import FormButton from "../../components/form-button/FormButton"
-import { services } from "../../utils/constants"
+import { services, theme } from "../../utils/constants"
 import { SelectOption } from "../../utils/types"
 
 export default function Page(): React.ReactElement | null {
 	const router = useRouter()
+
+	const [fontsLoaded] = useFonts({
+		"Montserrat-Bold": require("../../assets/fonts/Montserrat/Montserrat Bold 700.ttf")
+	})
 
 	const [jobTitle, setJobTitle] = useState<string>("")
 	const [jobType, setJobType] = useState<SelectOption | null>(null)
@@ -18,40 +25,78 @@ export default function Page(): React.ReactElement | null {
 	}, [router])
 
 	return (
-		<View style={styles.bodyContainer}>
-			<InputField
-				type="text"
-				value={jobTitle}
-				onChangeText={setJobTitle}
-				title="Job Title"
-				multiline={false}
-				secureTextEntry={false}
-				placeholder="Enter Job Title"
-			/>
-			<InputField
-				type="select"
-				data={services}
-				value={jobType}
-				onChangeValue={setJobType}
-				title="Job Type"
-				placeholder="Select Job Type"
-			/>
-			<InputField
-				type="text"
-				value={jobDescription}
-				onChangeText={setJobDescription}
-				title="Job Description"
-				multiline={true}
-				secureTextEntry={false}
-				placeholder="Write Job Description"
-				size="large"
-			/>
-			<FormButton title="Next" onPress={handleSubmit} />
+		<View style={styles.container}>
+			<ImageBackground
+				source={require("../../assets/images/add-job-header.png")}
+				style={styles.headerBackgroundImage}
+				contentFit="fill"
+			>
+				<View style={styles.headerContainer}>
+					<BackButton
+						color="#000000"
+						backgroundColor="#F5F5F5"
+						borderColor="transparent"
+					/>
+					{fontsLoaded && (
+						<Text style={styles.titleText}>
+							Describe Your Job Needs
+						</Text>
+					)}
+				</View>
+			</ImageBackground>
+			<View style={styles.bodyContainer}>
+				<InputField
+					type="text"
+					value={jobTitle}
+					onChangeText={setJobTitle}
+					title="Job Title"
+					multiline={false}
+					secureTextEntry={false}
+					placeholder="Enter Job Title"
+				/>
+				<InputField
+					type="select"
+					data={services}
+					value={jobType}
+					onChangeValue={setJobType}
+					title="Job Type"
+					placeholder="Select Job Type"
+				/>
+				<InputField
+					type="text"
+					value={jobDescription}
+					onChangeText={setJobDescription}
+					title="Job Description"
+					multiline={true}
+					secureTextEntry={false}
+					placeholder="Write Job Description"
+					size="large"
+				/>
+				<FormButton title="Next" onPress={handleSubmit} />
+			</View>
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
+	container: {
+		flexDirection: "column"
+	},
+	headerBackgroundImage: {
+		width: "100%"
+	},
+	headerContainer: {
+		padding: 25,
+		flexDirection: "column",
+		gap: 25,
+		backgroundColor: "rgba(255, 255, 255, 0.75)"
+	},
+	titleText: {
+		fontSize: 27.5,
+		fontFamily: "Montserrat-Bold",
+		color: theme.colors.secondary,
+		width: 215
+	},
 	bodyContainer: {
 		flexDirection: "column",
 		alignItems: "center",
