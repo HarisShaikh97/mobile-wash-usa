1229c840d96618b7738546a516ad45cfad388ae1
HarisShaikh97 <harisahmedsheikh1997@gmail.com>
2024-12-27 00:56:25 +0500

    Web customer job rating and review screen designed

---
+++ app/user/rating-and-review/[id]/index.web.tsx
@@ -0,0 +1,142 @@
+import { useState, useCallback } from "react"
+import { View, Text, StyleSheet } from "react-native"
+import { Image } from "expo-image"
+import { useLocalSearchParams } from "expo-router"
+import RatingsInput from "../../../../components/ratings-input/RatingsInput"
+import InputField from "../../../../components/input-field/InputField"
+import FormButton from "../../../../components/form-button/FormButton"
+import FeedbackConfirmationModal from "../../../../components/feedback-confirmation-modal/FeedbackConfirmationModal"
+import { theme } from "../../../../utils/constants"
+
+export default function Page(): React.ReactElement | null {
+	const { id } = useLocalSearchParams()
+
+	const [ratings, setRatings] = useState<number>(0)
+	const [review, setReview] = useState<string>("")
+	const [openModal, setOpenModal] = useState<boolean>(false)
+
+	const handleSubmit = useCallback((): void => {
+		setOpenModal(true)
+	}, [setOpenModal])
+
+	return (
+		<View style={styles.container}>
+			<FeedbackConfirmationModal
+				openModal={openModal}
+				setOpenModal={setOpenModal}
+			/>
+			<Image
+				source={require("../../../../assets/icons/successful.svg")}
+				style={styles.checkIcon}
+				contentFit="contain"
+			/>
+			<Text style={styles.titleText}>Your Job Has Been Completed!</Text>
+			<View style={styles.jobCardContainer}>
+				<Text
+					style={styles.jobTitleText}
+					numberOfLines={2}
+					ellipsizeMode="tail"
+				>
+					Car Wash Service Needed
+				</Text>
+				<Text style={styles.budgetText}>$500</Text>
+			</View>
+			<Text style={styles.experienceTitleText}>
+				How was your experience?
+			</Text>
+			<Text style={styles.descriptionText}>
+				Your feedback helps us ensure quality service. Rate and review
+				below.
+			</Text>
+			<RatingsInput size={35} ratings={ratings} setRatings={setRatings} />
+			<View style={styles.reviewBoxWrapper}>
+				<InputField
+					length="full"
+					type="text"
+					value={review}
+					onChangeText={setReview}
+					title="Write Your Review"
+					multiline={true}
+					secureTextEntry={false}
+					placeholder="Write your review..."
+					size="large"
+				/>
+			</View>
+			<View style={styles.formButtonWrapper}>
+				<FormButton
+					length="full"
+					theme="dark"
+					title="Submit"
+					onPress={handleSubmit}
+				/>
+			</View>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: "column",
+		alignItems: "center",
+		gap: 15,
+		paddingTop: 65,
+		paddingBottom: 35
+	},
+	checkIcon: {
+		height: 115,
+		width: 115
+	},
+	titleText: {
+		fontSize: 27.5,
+		fontFamily: "Montserrat-Bold",
+		color: theme.colors.secondary,
+		textAlign: "center",
+		textTransform: "capitalize"
+	},
+	jobCardContainer: {
+		width: "100%",
+		borderRadius: 15,
+		borderWidth: 1,
+		borderColor: "#F5F5F5",
+		backgroundColor: "white",
+		padding: 20,
+		flexDirection: "row",
+		alignItems: "flex-start",
+		justifyContent: "space-between"
+	},
+	jobTitleText: {
+		fontSize: 17.5,
+		fontFamily: "Montserrat-Bold",
+		width: 165,
+		color: theme.colors.secondary,
+		textTransform: "capitalize"
+	},
+	budgetText: {
+		fontSize: 22.5,
+		fontFamily: "Roboto-Bold",
+		color: theme.colors.primary
+	},
+	experienceTitleText: {
+		fontSize: 27.5,
+		fontFamily: "Montserrat-SemiBold",
+		width: 215,
+		color: theme.colors.secondary,
+		textAlign: "center",
+		textTransform: "capitalize"
+	},
+	descriptionText: {
+		fontSize: 15,
+		fontFamily: "Roboto-Regular",
+		width: 275,
+		color: theme.colors.secondary,
+		textAlign: "center"
+	},
+	reviewBoxWrapper: {
+		width: "100%",
+		marginVertical: 25
+	},
+	formButtonWrapper: {
+		width: "75%"
+	}
+})
