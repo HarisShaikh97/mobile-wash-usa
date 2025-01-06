d357174aeeb2c181020e42c0261d1998bbf4b82c
HarisShaikh97 <harisahmedsheikh1997@gmail.com>
2025-01-07 02:35:04 +0500

    Web vendor notifications screen designed

---
+++ app/vendor/notifications/index.web.tsx
@@ -0,0 +1,113 @@
+import { useState } from "react"
+import { View, Text, StyleSheet } from "react-native"
+import NotificationCard from "../../../components/notification-card/NotificationCard"
+import NotificationActionsModal from "../../../components/notification-actions-modal/NotificationActionsModal"
+import { theme } from "../../../utils/constants"
+import { Notification } from "../../../utils/types"
+
+export default function Page(): React.ReactElement | null {
+	const [openModal, setOpenModal] = useState<boolean>(false)
+
+	const notifications: Notification[] = [
+		{
+			_id: "1",
+			type: "message",
+			title: "Message",
+			description:
+				"You have a new message from John. Open the app to continue the conversation.",
+			time: "1 hour ago"
+		},
+		{
+			_id: "2",
+			type: "offer",
+			title: "Offer",
+			description: "Vendor Jane Doe bid $45 on your Car Wash job.",
+			time: "2 hour ago"
+		},
+		{
+			_id: "3",
+			type: "security",
+			title: "Password Reset Request",
+			description:
+				"Hi John a, we received a request to reset your password. Click here to create a new one: Link. If you didn’t make this request, please contact support.",
+			time: "3 hour ago"
+		}
+	]
+
+	return (
+		<View style={styles.container}>
+			<NotificationActionsModal
+				openModal={openModal}
+				setOpenModal={setOpenModal}
+				mode="web"
+			/>
+			<View style={styles.sectionContainer}>
+				<Text style={styles.sectionTitleText}>Today</Text>
+				<View style={styles.notificationCardsWrapper}>
+					{notifications.map(
+						(notification, index): React.ReactElement | null => {
+							return (
+								<NotificationCard
+									theme={index % 2 === 0 ? "dark" : "light"}
+									type={notification.type}
+									title={notification.title}
+									description={notification.description}
+									time={notification.time}
+									setOpenModal={setOpenModal}
+									mode="web"
+									key={index}
+								/>
+							)
+						}
+					)}
+				</View>
+			</View>
+			<View style={styles.sectionContainer}>
+				<Text style={styles.sectionTitleText}>Yesterday</Text>
+				<View style={styles.notificationCardsWrapper}>
+					{notifications.map(
+						(notification, index): React.ReactElement | null => {
+							return (
+								<NotificationCard
+									theme={index % 2 === 0 ? "dark" : "light"}
+									type={notification.type}
+									title={notification.title}
+									description={notification.description}
+									time={notification.time}
+									setOpenModal={setOpenModal}
+									mode="web"
+									key={index}
+								/>
+							)
+						}
+					)}
+				</View>
+			</View>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		width: "100%",
+		flexDirection: "column",
+		gap: 25
+	},
+	sectionContainer: {
+		width: "100%",
+		flexDirection: "column",
+		gap: 25
+	},
+	sectionTitleText: {
+		fontSize: 20,
+		fontFamily: "Montserrat-SemiBold",
+		color: theme.colors.secondary,
+		marginLeft: 15,
+		letterSpacing: 0.5
+	},
+	notificationCardsWrapper: {
+		width: "100%",
+		flexDirection: "column",
+		gap: 10
+	}
+})
