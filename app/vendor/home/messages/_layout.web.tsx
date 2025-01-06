9c940c0bfd558c34a6b36ee79bfc264b2413257d
HarisShaikh97 <harisahmedsheikh1997@gmail.com>
2025-01-07 02:27:41 +0500

    Web vendor messages screen designed

---
+++ app/vendor/home/messages/_layout.web.tsx
@@ -0,0 +1,26 @@
+import { View, StyleSheet } from "react-native"
+import { Slot } from "expo-router"
+import { WEB_SIDE_NAV_WIDTH } from "../../../../utils/constants"
+
+export default function Layout(): React.ReactElement | null {
+	return (
+		<View style={styles.wrapper}>
+			<View style={styles.container}>
+				<Slot />
+			</View>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	wrapper: {
+		flex: 1,
+		backgroundColor: "#F3F8FE",
+		paddingLeft: WEB_SIDE_NAV_WIDTH
+	},
+	container: {
+		flex: 1,
+		flexDirection: "column",
+		padding: 35
+	}
+})
