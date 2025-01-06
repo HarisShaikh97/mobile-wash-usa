94754e71e25f13c0de89cf6fdd5a1cab9b9bbea3
HarisShaikh97 <harisahmedsheikh1997@gmail.com>
2025-01-07 01:29:14 +0500

    Web vendor job completion otp screen designed

---
+++ app/vendor/job-completion-verification/[id]/_layout.web.tsx
@@ -0,0 +1,46 @@
+import { View, StyleSheet } from "react-native"
+import { ImageBackground } from "expo-image"
+import { Slot } from "expo-router"
+import BackButton from "../../../../components/back-button/BackButton"
+
+export default function Layout(): React.ReactElement | null {
+	return (
+		<ImageBackground
+			source={require("../../../../assets/images/sign-up-bg-web.png")}
+			style={styles.container}
+			contentFit="fill"
+		>
+			<BackButton
+				size="large"
+				color="#000000"
+				backgroundColor="#ffffff"
+				borderColor="transparent"
+			/>
+			<View style={styles.bodyContainer}>
+				<View style={styles.cardContainer}>
+					<Slot />
+				</View>
+			</View>
+		</ImageBackground>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#F3F8FE",
+		flexDirection: "column",
+		padding: 35
+	},
+	bodyContainer: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	cardContainer: {
+		width: 500,
+		backgroundColor: "white",
+		borderRadius: 25,
+		paddingVertical: 50
+	}
+})
---
+++ app/vendor/job-completion-verification/[id]/index.web.tsx
@@ -0,0 +1,85 @@
+import { useCallback, useState } from "react"
+import { View, Text, StyleSheet } from "react-native"
+import FormButton from "../../../../components/form-button/FormButton"
+import OTPInput from "../../../../components/otp-input/OTPInput"
+import JobCompletionSuccessfulModal from "../../../../components/job-completion-successful-modal/JobCompletionSuccessfulModal"
+import InvalidOTPModal from "../../../../components/invalid-otp-modal/InvalidOTPModal"
+import { theme } from "../../../../utils/constants"
+
+export default function Page(): React.ReactElement | null {
+	const [OTP, setOTP] = useState<string>("")
+	const [openSuccessModal, setOpenSuccessModal] = useState<boolean>(false)
+	const [openInvalidModal, setOpenInvalidModal] = useState<boolean>(false)
+
+	const handleSubmit = useCallback((): void => {
+		setOpenInvalidModal(true)
+	}, [setOpenSuccessModal, setOpenInvalidModal])
+
+	return (
+		<View style={styles.bodyContainer}>
+			<JobCompletionSuccessfulModal
+				openModal={openSuccessModal}
+				setOpenModal={setOpenSuccessModal}
+			/>
+			<InvalidOTPModal
+				openModal={openInvalidModal}
+				setOpenModal={setOpenInvalidModal}
+			/>
+			<Text style={styles.titleText}>Job Completion Verification</Text>
+			<Text style={[styles.descriptionText, styles.descriptionTextLarge]}>
+				Please enter the OTP code provided by the customer to confirm
+				the completion of the job.
+			</Text>
+			<View style={styles.formContainer}>
+				<OTPInput onChangeText={setOTP} />
+				<FormButton
+					length="full"
+					theme="dark"
+					title="Submit"
+					onPress={handleSubmit}
+				/>
+			</View>
+			<Text style={[styles.descriptionText, styles.descriptionTextSmall]}>
+				Once you enter the correct OTP, the job will be marked as
+				complete, and payment will be processed if applicable.
+			</Text>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	bodyContainer: {
+		flexDirection: "column",
+		alignItems: "center",
+		gap: 5
+	},
+	titleText: {
+		fontFamily: "Montserrat-Bold",
+		fontSize: 40,
+		color: theme.colors.secondary,
+		textAlign: "center",
+		width: 335,
+		lineHeight: 45
+	},
+	descriptionText: {
+		fontFamily: "Roboto-Regular",
+		color: theme.colors.secondary,
+		textAlign: "center",
+		letterSpacing: 0.5
+	},
+	descriptionTextLarge: {
+		fontSize: 18.5,
+		width: 315
+	},
+	descriptionTextSmall: {
+		fontSize: 16.5,
+		width: 365
+	},
+	formContainer: {
+		width: 300,
+		flexDirection: "column",
+		alignItems: "center",
+		gap: 25,
+		marginVertical: 15
+	}
+})
